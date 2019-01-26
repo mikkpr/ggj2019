c31f9a5f0ed8cc0b30e038442ee5db455ab94a1c
Mikk Pristavka <mikk.pristavka@reach-u.com>
2019-01-26 17:28:18 +0200

    Add basic goal scene, signal Main node when player reaches it

---
+++ Scripts/Goal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+signal goal_reached
+
+func _ready():
+	pass
+
+
+func _on_Goal_body_entered(body):
+	if body.is_in_group('player'):
+		emit_signal('goal_reached')
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,6 +5,7 @@ const MAX_FEAR = 100
 const FEAR_INCREMENT = 5
 
 var fear = INITIAL_FEAR
+var goal_reached = false
 
 onready var bar = $UILayer/BraveryBar
 
@@ -16,7 +17,7 @@ func _physics_process(delta):
 		self.update_fear(FEAR_INCREMENT)
 
 	if Input.is_action_just_pressed("ui_pause"):
-		if fear < MAX_FEAR:
+		if fear < MAX_FEAR or not goal_reached:
 			get_tree().paused = !get_tree().paused
 
 func update_bravery_bar():
@@ -31,8 +32,17 @@ func update_fear(amount):
 
 	if fear >= MAX_FEAR:
 		print("You are too afraid to continue")
-		get_tree().paused = true
+		pause_game()
+
+func pause_game():
+	get_tree().paused = true
 
 
 func _on_Monster_induce_fear():
 	update_fear(FEAR_INCREMENT)
+
+
+func _on_Goal_goal_reached():
+	goal_reached = true
+	pause_game()
+	print("Hooray! You did it!")
