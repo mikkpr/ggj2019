850703a894582e1edc560ec1dc0c4217f036f97f
Tiit Pikma <thsnr@users.noreply.github.com>
2019-01-26 21:49:01 +0200

    Spawn monsters from Main script

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,15 +1,20 @@
 extends Node
 
+export (PackedScene) var monster
+
 const INITIAL_FEAR = 0
 const MAX_FEAR = 100
 const FEAR_INCREMENT = 5
+const SPAWN_CHANCE = 5
 
 var fear = INITIAL_FEAR
 var goal_reached = false
 
 onready var bar = $UILayer/BraveryBar
+onready var player = find_node("Player")
 
 func _ready():
+	randomize()
 	self.update_bravery_bar()
 	
 func _physics_process(delta):
@@ -19,6 +24,13 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_pause"):
 		if fear < MAX_FEAR or not goal_reached:
 			get_tree().paused = !get_tree().paused
+
+	if player:
+		if randi() % 100 < SPAWN_CHANCE:
+			var mob = monster.instance()
+			add_child(mob)
+			mob.connect("induce_fear", self, "_on_Monster_induce_fear")
+			mob.position = player.position + Vector2(500, 0).rotated(randi())
 
 func update_bravery_bar():
 	var bar = $Node/UILayer/BraveryBar/Container/ProgressBar
