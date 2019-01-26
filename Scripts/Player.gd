8d9e7f4e2ec0adb5753b11d9ea719e8b84553341
Mikk Pristavka <mikk.pristavka@reach-u.com>
2019-01-26 20:01:18 +0200

    Player: add walking and idle animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,23 @@
 extends KinematicBody2D
 
 const LIGHT_DISTANCE = 10
+const SPEED = 250
 
-var speed = 250
-var velocity = Vector2()
 
 onready var lightpivot = $Lightpivot
 onready var light = $Lightpivot/Light
 onready var glow = $Lightpivot/Glow
 
+enum states {
+	STANDING,
+	WALKING
+}
+
+var state = STANDING
+var current_speed = 0
+
+var velocity = Vector2()
+var last_move_direction = Vector2(0, 1)
 
 func _ready():
 	for monster in $Lightpivot/Area2D.get_overlapping_bodies():
@@ -22,30 +31,99 @@ func _physics_process(delta):
 	lightpivot.look_at(get_global_mouse_position())
 	
 	get_input()
-	move_and_collide(velocity * delta)
-	
-	var sprite_size = Vector2(32, 32)
-	var world_limit_x = 1232
-	var world_limit_y = 768
-	
-	#position.x = clamp(position.x, 0 + sprite_size.x / 2, world_limit_x - sprite_size.x / 2)
-	#position.y = clamp(position.y, 0 + sprite_size.y / 2, world_limit_y - sprite_size.y / 2)
+	move_and_slide(velocity)
 
 
 func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+	if (
+		Input.is_action_pressed('ui_right') or 
+		Input.is_action_pressed('ui_left') or 
+		Input.is_action_pressed('ui_down') or 
+		Input.is_action_pressed('ui_up')
+	):
+		velocity = Vector2()
+		if Input.is_action_pressed('ui_right'):
+			velocity.x = 1
+		if Input.is_action_pressed('ui_left'):
+			velocity.x = -1
+		if Input.is_action_pressed('ui_down'):
+			velocity.y = 1
+		if Input.is_action_pressed('ui_up'):
+			velocity.y = -1
+		_set_state(WALKING)
+		last_move_direction = _get_cardinal_direction(velocity.angle())
+	else:
+		_set_state(STANDING)
+	
+	velocity = velocity.normalized() * current_speed
+
+
+func _get_cardinal_direction(angle):
+	if ((angle > 0 and angle < PI/4) or (angle <= 0 and angle > -PI/4)):
+		return Vector2(1, 0)
+	elif (angle >= PI/4 and angle <= 3*PI/4):
+		return Vector2(0, 1)
+	elif (angle <= -PI/4 and angle >= -3*PI/4):
+		return Vector2(0, -1)
+	else:
+		return Vector2(-1, 0)
+#
 
 func _on_Flashlight_body_entered(body):
 	body.detect(self)
 
 func _on_Flashlight_body_exited(body):
 	body.conceal()
+
+func _set_state(s):
+	if s == STANDING:
+		current_speed = 0
+		var animation = _get_standing_animation()
+		$Sprite.stop()
+		$Sprite.animation = animation
+		if animation == "idle_side":
+			$Sprite.flip_h = true if _get_side() == -1 else false
+		else:
+			$Sprite.flip_h = false
+		$Sprite.frame = 0
+
+	if state == s:
+		return
+
+	state = s
+	
+	if state == WALKING:
+		current_speed = SPEED
+		$Sprite.play(_get_walking_animation())
+		
+func _get_standing_animation():
+	print("standing")
+	var mouse_angle = get_local_mouse_position().normalized().angle()	
+	var mouse_direction = _get_cardinal_direction(mouse_angle)
+	print(last_move_direction, mouse_direction)
+
+	if (mouse_direction == Vector2(0, 1)):
+		return "idle_front"
+	elif (mouse_direction == Vector2(0, -1)):
+		return "idle_back"
+	elif (mouse_direction == Vector2(1, 0) or mouse_direction == Vector2(-1, 0)):
+		return "idle_side"
+		
+func _get_side():
+	var mouse_angle = get_local_mouse_position().normalized().angle()	
+	var mouse_direction = _get_cardinal_direction(mouse_angle)
+	if (mouse_direction == Vector2(1, 0)):
+		return 1
+	else:
+		return -1
+
+func _get_walking_animation():
+	var mouse_angle = get_local_mouse_position().normalized().angle()	
+	var mouse_direction = _get_cardinal_direction(mouse_angle)
+	print("mouse direction: %s" % [mouse_direction])
+	if (mouse_direction == Vector2(0, 1)):
+		return "walk_front"
+	elif (mouse_direction == Vector2(0, -1)):
+		return "walk_back"
+	elif (mouse_direction == Vector2(1, 0) or mouse_direction == Vector2(-1, 0)):
+		return "walk_side"
