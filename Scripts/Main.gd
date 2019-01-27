ae2365dd9c1332600323bbef0d40d5ed3d2f4856
Tiit Pikma <thsnr@users.noreply.github.com>
2019-01-27 13:06:04 +0200

    Trigger scene transitions from Main

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 export (PackedScene) var monster
+export (PackedScene) var home
+export (PackedScene) var success
+export (PackedScene) var game_over
 
 const INITIAL_FEAR = 0
 const MAX_FEAR = 100
@@ -45,7 +48,7 @@ func update_fear(amount):
 
 	if fear >= MAX_FEAR:
 		print("You are too afraid to continue")
-		pause_game()
+		_change_scene(game_over)
 
 func toggle_pause_state():
 	var paused_state = !get_tree().paused
@@ -66,6 +69,19 @@ func _on_Monster_induce_fear():
 
 
 func _on_Goal_goal_reached():
+	print("Trash taken out")
 	goal_reached = true
-	pause_game()
-	print("Hooray! You did it!")
+
+func _on_Home_goal_reached():
+	if goal_reached:
+		print("Good boy")
+		_change_scene(success)
+	else:
+		print("Get back out there")
+		_change_scene(home)
+
+func _change_scene(scene):
+	if not scene:
+		get_tree().reload_current_scene()
+	else:
+		get_tree().change_scene_to(scene)
