a73bef2c5c1fad5e4d2982706f9f108c344419f9
Aare Undo <aare.undo@gmail.com>
2019-01-27 12:32:32 +0200

    clear label after n seconds

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -143,9 +143,21 @@ func _get_mother_text_node():
 func announce(text):
 	var label = _get_announcement_text_node()
 	label.text = text
+	_clear_label_after_seconds(label, 3)
 
 func mother_say(text):
 	var label = _get_mother_text_node()
-	label.text = text	
+	label.text = text
+	_clear_label_after_seconds(label, 3)
+	
+func _clear_label_after_seconds(label, seconds):
+	var t = Timer.new()
+	t.set_wait_time(3)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	label.text = ""
+	t.queue_free()
 
 
